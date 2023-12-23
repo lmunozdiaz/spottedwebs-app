b65a9520d289b538f0da1b3b7207f1d956c2d3e4
Luis Munoz Diaz <luismunozdiaz@icloud.com>
2023-12-23 14:44:40 -0600

    delete: remove entire api; remake with javascript

--- api/src/config/db/scripts/drop-tables.sql
+++
@@ -1,9 +0,0 @@
-USE `spottedwebsdb`;
-
-DROP TABLE IF EXISTS `image_collection`;
-
-DROP TABLE IF EXISTS `collection`;
-
-DROP TABLE IF EXISTS `image`;
-
-DROP TABLE IF EXISTS `user`;
--- api/src/config/multer/multer.ts
+++
@@ -1,4 +0,0 @@
-import multer from "multer";
-
-export const storage = multer.memoryStorage();
-export const upload = multer({ storage: storage });
