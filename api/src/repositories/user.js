2d355de8dec1bc8ef15853456f1f33e7ae1c54c9
Luis Munoz Diaz <luismunozdiaz@icloud.com>
2023-12-25 19:45:59 -0600

    added try-catch block

--- api/src/repositories/user.js
+++ api/src/repositories/user.js
@@ -1,15 +1,23 @@
 import { pool } from "@config/db/database";
 
 export async function insertOne(username, email, password) {
-  const statement =
-    "INSERT INTO User (username, email, password) VALUES (?, ?, ?);";
-  const [result] = await pool.query(statement, [username, email, password]);
-  const userId = result.insertId;
-  return await findById(userId);
+  try {
+    const statement =
+      "INSERT INTO User (username, email, password) VALUES (?, ?, ?);";
+    const [result] = await pool.query(statement, [username, email, password]);
+    const { insertId } = result;
+    return await findById(insertId);
+  } catch (error) {
+    console.log("Query error:", error);
+  }
 }
 
 export async function findByUsername(username) {
-  const statement = "SELECT * FROM User WHERE username = ?;";
-  const [result] = await pool.query(statement, [username]);
-  return result;
+  try {
+    const statement = "SELECT * FROM User WHERE username = ?;";
+    const [result] = await pool.query(statement, [username]);
+    return result;
+  } catch (error) {
+    console.log("Query error:", error);
+  }
 }
