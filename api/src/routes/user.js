2f7b5680515fc865f976b82251c309244bc2118a
Luis Munoz Diaz <luismunozdiaz@icloud.com>
2024-01-02 11:13:57 -0600

    learning public and private routes

--- api/src/routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser, getUser, loginUser, logoutUser } from "@controllers/user";
-
-export const router = express.Router();
-
-router.post("/register", createUser);
-router.post("/login", loginUser);
-router.post("/logout", logoutUser);
-router.get("/:username", getUser);
